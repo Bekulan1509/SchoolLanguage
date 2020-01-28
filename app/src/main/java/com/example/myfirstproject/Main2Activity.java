79724152fecc7f0cda92b8e65c0a37348599c189
Bekulan <totonya1509@bk.ru>
2020-01-28 19:21:54 +0600

    intent

---
+++ app/src/main/java/com/example/myfirstproject/Main2Activity.java
@@ -0,0 +1,42 @@
+package com.example.myfirstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        button = findViewById(R.id.button);
+        button.setBackgroundColor(Color.TRANSPARENT);
+        button.setTextColor(Color.WHITE);
+
+
+        MainThread mainThread = new MainThread();
+        mainThread.start();
+    }
+
+    public class MainThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            try {
+                MainThread.sleep(2800);
+                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(intent);
+
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/myfirstproject/MainActivity.java
+++ app/src/main/java/com/example/myfirstproject/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.myfirstproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -10,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 
 import java.util.Random;
 
@@ -19,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     EditText name1;
     EditText name2;
     Button button;
-    MediaPlayer buttonSound,buttonSound2;
+    MediaPlayer buttonSound, buttonSound2, buttonSound3;
 
 
     @Override
@@ -31,92 +30,105 @@ public class MainActivity extends AppCompatActivity {
         name2 = findViewById(R.id.name2);
         button = findViewById(R.id.buttonAnswer);
 
-        button.setText("получить прогноз");
-        button.setTextColor(Color.BLACK);
-        button.setBackgroundColor(Color.GREEN);
+
+        button.setTextColor(getResources().getColor(R.color.colorPrimary));
+        button.setBackgroundColor(getResources().getColor(R.color.BLUE));
+
 
         buttonSound = MediaPlayer.create(this, R.raw.loosers);
-        buttonSound2 = MediaPlayer.create(this,R.raw.win);
+        buttonSound2 = MediaPlayer.create(this, R.raw.winer);
+        buttonSound3 = MediaPlayer.create(this,R.raw.super_winer);
 
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String text = name1.getText().toString();
-                String text2 = name2.getText().toString();
+                final String text = name1.getText().toString();
+                final String text2 = name2.getText().toString();
 
 
                 Random random = new Random();
-                final int runNum = random.nextInt(100);
+                int runNum = random.nextInt(100);
 
 
                 if (text.equals("Bekulan") && text2.equals("Tahmina")) {
-                    Toast.makeText(MainActivity.this, "процент вашей совместимости 100%" + "     Вы будете отличной парой 👍", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "процент вашей совместимости 100%" + "     Вы созданы для друг друга 💘", Toast.LENGTH_LONG).show();
 
                 } else if (text.equals("Tahmina") && text2.equals("Bekulan")) {
-                    Toast.makeText(MainActivity.this, "процент вашей совместимости 100%" + "     Вы будете отличной парой 👍", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "процент вашей совместимости 100%" + "    Вы созданы для друг друга 💘", Toast.LENGTH_LONG).show();
 
                 } else if (text.equals("tahmina") && text2.equals("bekulan")) {
-                    Toast.makeText(MainActivity.this, "процент вашей совместимости 100%" + "     Вы будете отличной парой 👍", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "процент вашей совместимости 100%" + "    Вы созданы для друг друга 💘", Toast.LENGTH_LONG).show();
 
                 } else if (text.equals("bekulan") && text2.equals("tahmina")) {
-                    Toast.makeText(MainActivity.this, "процент вашей совместимости 100%" + "     Вы будете отличной парой 👍", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "процент вашей совместимости 100%" + "    Вы созданы для друг друга 💘", Toast.LENGTH_LONG).show();
 
                 } else if (text.equals("Бекулан") && text2.equals("Тахмина")) {
-                    Toast.makeText(MainActivity.this, "процент вашей совместимости 100%" + "     Вы будете отличной парой 👍", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "процент вашей совместимости 100%" + "     Вы созданы для друг друга 💘", Toast.LENGTH_LONG).show();
 
                 } else if (text.equals("Тахмина") && text2.equals("Бекулан")) {
-                    Toast.makeText(MainActivity.this, "процент вашей совместимости 100%" + "     Вы будете отличной парой 👍", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "процент вашей совместимости 100%" + "     Вы созданы для друг друга 💘", Toast.LENGTH_LONG).show();
 
                 } else if (text.equals("тахмина") && text2.equals("бекулан")) {
-                    Toast.makeText(MainActivity.this, "процент вашей совместимости 100%" + "     Вы будете отличной парой 👍", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "процент вашей совместимости 100%" + "     Вы созданы для друг друга 💘", Toast.LENGTH_LONG).show();
+
+                } else if (text.equals("1") || text.equals("2") || text.equals("3") || text.equals("4") || text.equals("5") || text.equals("6") || text.equals("7") || text.equals("8") || text.equals("9") || text.equals("0")) {
+                    Toast.makeText(MainActivity.this, "уберите цифры!", Toast.LENGTH_LONG).show();
 
                 } else if (text.equals("бекулан") && text2.equals("тахмина")) {
-                    Toast.makeText(MainActivity.this, "процент вашей совместимости 100%" + "     Вы будете отличной парой 👍", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "процент вашей совместимости 100%" + "    Вы созданы для друг друга 💘", Toast.LENGTH_LONG).show();
 
-                } else if (runNum < 50) {
-                    Toast.makeText(MainActivity.this, "Процент вашей совместимости: " + runNum + "%", Toast.LENGTH_LONG).show();
-
-                } else if (runNum > 50) {
-                    Toast.makeText(MainActivity.this, "Процент вашей совместимости: " + runNum + "%" + "     Вы будете хорошей парой \uD83D\uDC4D", Toast.LENGTH_LONG).show();
-                    buttonSound2.start();
-
-                }
-                if (text.equals("") && text2.equals("")) {
-                    Toast.makeText(MainActivity.this, " ведите  двe имени!", LENGTH_SHORT).show();
+                } else if (text.equals("") && text2.equals("")) {
+                    Toast.makeText(MainActivity.this, " Введите  двe имени!", LENGTH_SHORT).show();
 
                 } else if (text.equals("")) {
-                    Toast.makeText(MainActivity.this, " ведите первое имя!", LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, " Введите первое имя!", LENGTH_SHORT).show();
+
                 } else if (text2.equals("")) {
-                    Toast.makeText(MainActivity.this, " ведите второе имя!", LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, " Введите второе имя!", LENGTH_SHORT).show();
+
+                } else if (runNum < 50) {
+                    Toast.makeText(MainActivity.this, "Процент вашей совместимости: " + runNum + "%   👎", Toast.LENGTH_LONG).show();
+                    buttonSound.start();
+
+                } else if (runNum > 50 && runNum < 80) {
+                    Toast.makeText(MainActivity.this, "Процент вашей совместимости: " + runNum + "%" + "     Вы будете хорошей парой 💞", Toast.LENGTH_LONG).show();
+                    buttonSound2.start();
+
+                } else if (runNum > 80) {
+                    Toast.makeText(MainActivity.this, "Процент вашей совместимости: " + runNum + "%" + "     Вы созданы друг для друга 💘", Toast.LENGTH_LONG).show();
+                    buttonSound3.start();
+                }
+                if (text.equals("") && text2.equals("")) {
+                    Toast.makeText(MainActivity.this, " Введите  двe имени!", LENGTH_SHORT).show();
+                } else if (text.equals("")) {
+                    Toast.makeText(MainActivity.this, " Введите первое имя!", LENGTH_SHORT).show();
+                } else if (text2.equals("")) {
+                    Toast.makeText(MainActivity.this, " Введите второе имя!", LENGTH_SHORT).show();
+                } else if (text.equals("1") || text.equals("2 ") || text.equals("3") || text.equals("4") || text.equals("5") || text.equals("6") || text.equals("7") || text.equals("8") || text.equals("9") || text.equals("0")) {
+                    Toast.makeText(MainActivity.this, "уберите цифры!", Toast.LENGTH_LONG).show();
                 } else
-                    new CountDownTimer(2500, 10) {
+                    new CountDownTimer(4000, 10) {
                         @Override
                         public void onTick(long l) {
-                            if (runNum<50){
-                                buttonSound.start();
-                            }else if (runNum>=50){buttonSound2.start();}
-
                             button.setText("✔");
-                            button.setBackgroundColor(Color.GREEN);
+                            button.setBackgroundColor(getResources().getColor(R.color.green));
                             button.setEnabled(false);
-
-
                         }
 
                         @Override
                         public void onFinish() {
                             button.setText("получить прогноз");
-                            button.setBackgroundColor(Color.GREEN);
+                            button.setBackgroundColor(getResources().getColor(R.color.BLUE));
                             button.setEnabled(true);
-
-
                         }
+
+
                     }.start();
 
-            }
 
+            }
         });
 
     }
